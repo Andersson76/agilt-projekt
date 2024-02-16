5ed79800fab63c2f2a83b1d575ea7307b1c5ab2e
Martin <106612709+menkodot@users.noreply.github.com>
2024-02-16 15:14:25 +0100

    edits to header

--- group-project/src/components/HeaderMenu.vue
+++ group-project/src/components/HeaderMenu.vue
@@ -3,7 +3,7 @@
     <div>
       <MenuButton
         class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-        Options
+        Menu
         <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
       </MenuButton>
     </div>
@@ -15,10 +15,6 @@
         class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
         <div class="py-1">
           <MenuItem v-slot="{ active }">
-          <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Account
-            settings</a>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
           <a href="#"
             :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Support</a>
           </MenuItem>
@@ -26,19 +22,30 @@
           <a href="#"
             :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">License</a>
           </MenuItem>
-          <form method="POST" action="#">
-            <MenuItem v-slot="{ active }">
-            <button type="submit"
-              :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-left text-sm']">Sign
-              out</button>
-            </MenuItem>
-          </form>
+          <!-- <form method="POST" action="#"> -->
+          <MenuItem v-slot="{ active }">
+          <button type="submit"
+            :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-left text-sm']"
+            @click="toggleLogin">{{ isLoggedIn ? 'Sign out' : 'Sign in' }}</button>
+          </MenuItem>
+          <!-- </form> -->
         </div>
       </MenuItems>
-  </transition>
-</Menu></template>
+    </transition>
+  </Menu>
+</template>
 
 <script setup>
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+
+import { ref } from 'vue';
+
+const isLoggedIn = ref(false);
+
+const toggleLogin = () => {
+  isLoggedIn.value = !isLoggedIn.value;
+  // Skicka ett custom event för att meddela förälderkomponenten om inloggningsstatusen
+  emit('loginStatusChanged', isLoggedIn.value);
+};
 </script>

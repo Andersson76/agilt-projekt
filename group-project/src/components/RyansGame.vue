f9f16f453f98f864529c68bb24ff6afe159fadcb
RyAndroid <ryan.w.simpson@gmail.com>
2024-02-23 09:01:38 +0100

    Game Update 2

--- group-project/src/components/RyansGame.vue
+++ group-project/src/components/RyansGame.vue
@@ -1,9 +1,10 @@
 <template>
-  <div ref="game"></div>
+  <div ref="game"/>
 </template>
 
 <script setup>
-import Phaser from "phaser";
+import { ref, onBeforeUnmount } from 'vue';
+import Phaser from 'phaser';
 
 let player;
 let stars;
@@ -11,8 +12,13 @@ let bombs;
 let platforms;
 let cursors;
 let score = 0;
+let level = 0;
 let gameOver = false;
 let scoreText;
+let levelText;
+let isLeftButtonDown = false;
+let isRightButtonDown = false;
+let isJumpButtonDown = false;
 
 const config = {
   type: Phaser.AUTO,
@@ -20,7 +26,7 @@ const config = {
   height: 600,
   autoCenter: true,
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       gravity: { y: 300 },
       debug: false,
@@ -35,115 +41,110 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+const gameContainer = ref(null);
+
 function preload() {
-  // this.load.image("sky", "/assets/RyansGame/sky.png");
-  // this.load.image("ground", "/assets/RyansGame/platform.png");
-  // this.load.image("star", "/assets/RyansGame/star.png");
-  // this.load.image("bomb", "/assets/RyansGame/bomb.png");
-  // this.load.spritesheet("dude", "/assets/RyansGame/dude.png",
-  this.load.image("sky", "sky.png");
-  this.load.image("ground", "platform.png");
-  this.load.image("star", "star.png");
-  this.load.image("bomb", "bomb.png");
-  this.load.spritesheet("dude", "dude.png",
-  {
+  this.load.image('sky', 'sky.png');
+  this.load.image('ground', 'platform.png');
+  this.load.image('star', 'star.png');
+  this.load.image('bomb', 'bomb.png');
+  this.load.spritesheet('dude', 'dude.png', {
     frameWidth: 31,
     frameHeight: 72,
   });
 }
 
 function create() {
-  const sky = this.add.image(400, 300, "sky");
+  const sky = this.add.image(400, 300, 'sky');
 
   platforms = this.physics.add.staticGroup();
 
-  platforms.create(400, 568, "ground").setScale(2).refreshBody();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-  platforms.create(600, 400, "ground");
-  platforms.create(50, 250, "ground");
-  platforms.create(750, 220, "ground");
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
 
-  player = this.physics.add.sprite(100, 450, "dude");
+  player = this.physics.add.sprite(100, 450, 'dude');
 
   player.setBounce(0.1);
   player.setCollideWorldBounds(true);
-// buttons----------------->
-  const buttonL = this.add.text(80, 570, '<', {
-            fontFamily: 'Arial',
-            fontSize: '32px',
-            color: '#ffffff',
-            align: 'center',
-            fixedWidth: 40,
-            fixedHeight:40,
-            backgroundColor: '#2d2d2d'
-        }).setPadding(3).setOrigin(0.5);
 
-        buttonL.setInteractive({ useHandCursor: true });
+  const buttonL = this.add.sprite(80, 570, 'buttonL').setInteractive();
+  const buttonR = this.add.sprite(130, 570, 'buttonR').setInteractive();
+  const buttonJump = this.add.sprite(680, 570, 'buttonJump').setInteractive();
 
-        buttonL.on('pointerover', () => {
-            buttonL.setBackgroundColor('#8d8d8d');
-        });
+  buttonL.setOrigin(0.5);
+  buttonR.setOrigin(0.5);
+  buttonJump.setOrigin(0.5);
 
-        buttonL.on('pointerout', () => {
-            buttonL.setBackgroundColor('#2d2d2d');
-        });
+  this.add.text(buttonL.x, buttonL.y, '<', {
+    fontFamily: 'Arial',
+    fontSize: '32px',
+    color: '#ffffff',
+    align: 'center',
+    fixedWidth: 40,
+    fixedHeight: 40,
+    backgroundColor: '#2d2d2d'
+  }).setPadding(3).setOrigin(0.5);
 
-        const buttonR = this.add.text(130, 570, '>', {
-            fontFamily: 'Arial',
-            fontSize: '32px',
-            color: '#ffffff',
-            align: 'center',
-            fixedWidth: 40,
-            fixedHeight:40,
-            backgroundColor: '#2d2d2d'
-        }).setPadding(3).setOrigin(0.5);
+  this.add.text(buttonR.x, buttonR.y, '>', {
+    fontFamily: 'Arial',
+    fontSize: '32px',
+    color: '#ffffff',
+    align: 'center',
+    fixedWidth: 40,
+    fixedHeight: 40,
+    backgroundColor: '#2d2d2d'
+  }).setPadding(3).setOrigin(0.5);
 
-        buttonR.setInteractive({ useHandCursor: true });
+  this.add.text(buttonJump.x, buttonJump.y, 'JUMP', {
+    fontFamily: 'Arial',
+    fontSize: '32px',
+    color: '#ffffff',
+    align: 'center',
+    fixedWidth: 100,
+    fixedHeight: 40,
+    backgroundColor: '#2d2d2d'
+  }).setPadding(3).setOrigin(0.5);
 
-        buttonR.on('pointerover', () => {
-            buttonR.setBackgroundColor('#8d8d8d');
-        });
+  buttonL.on('pointerdown', () => {
+    isLeftButtonDown = true;
+  });
+  buttonL.on('pointerup', () => {
+    isLeftButtonDown = false;
+  });
 
-        buttonR.on('pointerout', () => {
-            buttonR.setBackgroundColor('#2d2d2d');
-        });
+  buttonR.on('pointerdown', () => {
+    isRightButtonDown = true;
+  });
+  buttonR.on('pointerup', () => {
+    isRightButtonDown = false;
+  });
 
-        const buttonJump = this.add.text(680, 570, 'JUMP', {
-            fontFamily: 'Arial',
-            fontSize: '32px',
-            color: '#ffffff',
-            align: 'center',
-            fixedWidth: 100,
-            fixedHeight:40,
-            backgroundColor: '#2d2d2d'
-        }).setPadding(3).setOrigin(0.5);
+  buttonJump.on('pointerdown', () => {
+    isJumpButtonDown = true;
+  });
+  buttonJump.on('pointerup', () => {
+    isJumpButtonDown = false;
+  });
 
-        buttonJump.setInteractive({ useHandCursor: true });
-
-        buttonJump.on('pointerover', () => {
-            buttonJump.setBackgroundColor('#8d8d8d');
-        });
-
-        buttonJump.on('pointerout', () => {
-            buttonJump.setBackgroundColor('#2d2d2d');
-        });
-// keyboard input --------------->
   this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1,
   });
 
   this.anims.create({
-    key: "turn",
-    frames: [{ key: "dude", frame: 4 }],
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
     frameRate: 20,
   });
 
   this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
     frameRate: 10,
     repeat: -1,
   });
@@ -151,23 +152,29 @@ function create() {
   cursors = this.input.keyboard.createCursorKeys();
 
   stars = this.physics.add.group({
-    key: "star",
+    key: 'star',
     repeat: 8,
     setXY: { x: 12, y: 0, stepX: 90 },
   });
-// stars
+
   stars.children.iterate(function (child) {
     child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.4));
   });
 
   bombs = this.physics.add.group();
 
-  //  The score
-  scoreText = this.add.text(16, 16, "Score: 0", {
-    fontSize: "32px",
-    fill: "#000",
+  scoreText = this.add.text(16, 16, 'Score: 0', {
+    fontSize: '32px',
+    fill: '#ffffff',
+    backgroundColor: '#2d2d2d',
   });
-// ------------------------
+  levelText = this.add.text(16, 50, 'level: 0', {
+    fontSize: '32px',
+    fill: '#ffffff',
+    backgroundColor: '#2d2d2d',
+  });
+
+
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(stars, platforms);
   this.physics.add.collider(bombs, platforms);
@@ -182,18 +189,18 @@ function update() {
     return;
   }
 
-  if (cursors.left.isDown) {
+  if ((cursors.left.isDown || isLeftButtonDown) && !(cursors.right.isDown || isRightButtonDown)) {
     player.setVelocityX(-160);
-    player.anims.play("left", true);
-  } else if (cursors.right.isDown) {
+    player.anims.play('left', true);
+  } else if ((cursors.right.isDown || isRightButtonDown) && !(cursors.left.isDown || isLeftButtonDown)) {
     player.setVelocityX(160);
-    player.anims.play("right", true);
+    player.anims.play('right', true);
   } else {
     player.setVelocityX(0);
-    player.anims.play("turn");
+    player.anims.play('turn');
   }
 
-  if (cursors.up.isDown && player.body.touching.down) {
+  if ((cursors.up.isDown || isJumpButtonDown) && player.body.touching.down) {
     player.setVelocityY(-330);
   }
 }
@@ -201,9 +208,11 @@ function update() {
 function collectStar(player, star) {
   star.disableBody(true, true);
   score += 10;
-  scoreText.setText("Score: " + score);
+  scoreText.setText('Score: ' + score);
 
   if (stars.countActive(true) === 0) {
+    level += 1
+    levelText.setText('level: ' + level)
     stars.children.iterate(function (child) {
       child.enableBody(true, child.x, 0, true, true);
     });
@@ -213,18 +222,32 @@ function collectStar(player, star) {
         ? Phaser.Math.Between(400, 800)
         : Phaser.Math.Between(0, 400);
 
-    const bomb = bombs.create(x, 16, "bomb");
+    const bomb = bombs.create(x, 16, 'bomb');
     bomb.setBounce(1);
     bomb.setCollideWorldBounds(true);
     bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
     bomb.allowGravity = false;
+
   }
 }
 
 function hitBomb(player, bomb) {
   this.physics.pause();
   player.setTint(0xff0000);
-  player.anims.play("turn");
+  player.anims.play('turn');
   gameOver = true;
+  score = 0;
+  level = 0;
+  setTimeout(() => {
+    this.scene.stop();
+    setTimeout(() => {
+      this.scene.start();
+      gameOver = false;
+    }, 500);
+  }, 500);
 }
+
+onBeforeUnmount(() => {
+  game.destroy(true);
+});
 </script>

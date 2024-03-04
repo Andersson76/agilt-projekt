b020248cae4e86b674f2b1f42561caa889304607
Madchili <129065416+Madchili@users.noreply.github.com>
2024-03-04 14:18:14 +0100

    Added progress bar

--- group-project/src/main.js
+++ group-project/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).use(router).mount('#app')

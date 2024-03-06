555740fa83c44f3ed79598579ad5328d99d5c451
RyAndroid <ryan.w.simpson@gmail.com>
2024-03-06 14:14:02 +0100

    Changed names in navbr added why

--- group-project/src/router.js
+++ group-project/src/router.js
@@ -1,55 +1,59 @@
-import { createRouter, createWebHistory } from "vue-router";
-
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './Pages/Home.vue'
 import Achievement from './Pages/Achievement.vue'
 import Login from './Pages/Login.vue'
 import Adventure from './Pages/Adventure.vue'
 import BergsQuiz from './Pages/MartinQuiz.vue'
-import Andersson from "./components/Andersson.vue";
+import Andersson from './components/Andersson.vue'
 import ProductBoxPage from './Pages/ProductBoxPage.vue'
 import GameStatsPage from './Pages/GameStatsPage.vue'
-import MathTreasureApp from './MathTreasureApp.vue';
+import MathTreasureApp from './MathTreasureApp.vue'
+import Why from './Pages/why.vue'
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     {
       component: Home,
-      path: "/",
+      path: '/'
     },
     {
       component: Achievement,
-      path: "/Achievements",
+      path: '/Achievements'
     },
     {
       component: Login,
-      path: "/Login",
+      path: '/Login'
     },
     {
       component: Andersson,
-      path: "/Andersson",
+      path: '/Andersson'
     },
     {
       component: BergsQuiz,
-      path: '/Bergsquiz',
+      path: '/Bergsquiz'
     },
     {
       component: Adventure,
-      path: '/Adventure',
+      path: '/Adventure'
     },
     {
       component: ProductBoxPage,
-      path: '/ProductBox',
+      path: '/ProductBox'
     },
     {
       component: GameStatsPage,
-      path: '/StatisticsPage',
+      path: '/StatisticsPage'
     },
 
     {
       component: MathTreasureApp,
       path: '/MathTreasure'
+    },
+    {
+      component: Why,
+      path: '/Why'
     }
-  ],
-});
+  ]
+})
